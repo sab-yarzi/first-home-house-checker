45afce36072f75e25e0935da6163c815a9b2b538
sab.y <ed19.sy@leeds.ac.uk>
2025-11-19 20:53:19 +0000

    Adding a test flag - check if telegram works as expected

--- packages/monitor/check/__main__.py
+++ packages/monitor/check/__main__.py
@@ -4,7 +4,13 @@ import requests
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID =  os.getenv("CHAT_ID")
 
+def startup():
+    if not BOT_TOKEN or not CHAT_ID:
+        raise ValueError("BOT_TOKEN and CHAT_ID environment variables must be set.")
+
 def notify(msg: str):
+    startup()
+
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     try:
         requests.post(url, json={"chat_id": CHAT_ID, "text": msg}, timeout=10)
@@ -12,6 +18,12 @@ def notify(msg: str):
         print("Failed to send Telegram message:", e)
 
 def main(args):
+    ##Test notify
+    if args and args.get("test_notify") == "true":
+        notify("Test message from ShareToBuy monitor.")
+        return {"ok" : True, "mode": "Test"}
+
+
     url='https://www.sharetobuy.com/properties-count/?location=Hackney%20-%20Hackney&locationId=106452&lat=51.55492&lon=-0.060450000000000004&developmentId=&hasPolygon=1&userSearchId=&hereLocation=&hereLocation=%23%23%23hereLocation%23%23%23&locationId=106452&lat=51.55492&lon=-0.060450000000000004&developmentId=&hasPolygon=1&userSearchId=&radius=40&minBedrooms=&maxBedrooms=&schemeType%5B%5D=14&minMonthlyCost=&maxMonthlyCost=&minDeposit=&maxDeposit=&radius=40&rentOrBuy=2&createdAtPeriod=&minMinShareAvailable=&maxMinShareAvailable=&minFullMarketPrice=&maxFullMarketPrice=&viewType=1&campaignId=&developerId=&_token=f43dXRIWHwknKKWt1tLLanlXJPdnczEWdZDOhBYb&showAllPopular=showAllPopular&propertyTypeGroup=undefined&showAllOther=1&_token=f43dXRIWHwknKKWt1tLLanlXJPdnczEWdZDOhBYb&action=%2Fproperties-count%2F&_=1763550069528'
     response = None
     count_of_properties_available = 0
