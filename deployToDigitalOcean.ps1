64b8169296f825a3228f835fe1a2494b128f2005
sab.y <ed19.sy@leeds.ac.uk>
2025-11-19 20:43:26 +0000

    Fixing syntax for yml file and creating a powershell script for easy deployment to DO.

---
+++ deployToDigitalOcean.ps1
@@ -0,0 +1,22 @@
+if (Test-Path "./secrets.ps1") {
+    . ./secrets.ps1
+} else {
+    Write-Error "secrets.ps1 file not found. Please create the file with necessary secrets."
+    exit 1
+}
+
+
+
+if (-not (Get-Command doctl -ErrorAction SilentlyContinue)) {
+    Write-Error "doctl not found."
+    exit 1
+}
+
+doctl serverless deploy . --verbose
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Deployment failed."
+    exit $LASTEXITCODE
+}
+Write-Output "Deployment succeeded."
+
+doctl serverless invoke monitor/check
